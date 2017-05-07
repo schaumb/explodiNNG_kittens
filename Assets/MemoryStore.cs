ccd2b0be740de419e3d2ae783d2800b9257e2d35
Dobreff András <dobreffandras@gmail.com>
2017-05-07 19:06:57 +0200

    Added a memory store with the exsting boxes in the Scene

---
+++ Assets/MemoryStore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryStore : MonoBehaviour {
+
+    public GameObject[] Store;
+
+	// Use this for initialization
+	void Start () {
+		foreach (var box in Store)
+        {
+            Debug.Log(string.Format("Box is up and running name: {0}", box.name));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
